2a7c916a44911108701e4862a6e0d857505b5092
chrispanther1975 <59424834+chrispanther1975@users.noreply.github.com>
2020-03-07 21:34:36 +0000

    UnityConfigContainer

--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,9 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T : BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
-        ObjectCache cache =  MemoryCache.Default;
+        ObjectCache cache = MemoryCache.Default;
         List<T> items;
         string className;
 
@@ -27,7 +28,7 @@ namespace MyShop.DataAccess.InMemory
         {
             cache[className] = items;
         }
-
+      
         public void Insert(T t)
         {
             items.Add(t);
@@ -61,7 +62,7 @@ namespace MyShop.DataAccess.InMemory
         {
             return items.AsQueryable();
         }
-        public void Delete (string Id)
+        public void Delete(string Id)
         {
             T tToDelete = items.Find(i => i.Id == Id);
 
